0f87958c9521067770be7b84bb07d52276236fd9
Elena Yadryshnikova <elenaya014@gmail.com>
2023-02-03 23:06:57 +0700

    feat: add card

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,45 @@
 const editButton = document.querySelector('.profile__edit-button');
-const saveButton = document.querySelector('.popup__submit-button');
-const closeButton = document.querySelector('.popup__close-cross');
+const addButton = document.querySelector('.profile__add-button');
 
-const popup = document.querySelector('.popup');
+const popupProfile = document.querySelector('.popup_type_profile');
+const popupAddCard = document.querySelector('.popup_type_add-card');
+
+const closeButtonProfile = popupProfile.querySelector('.popup__close-cross');
+const closeButtonCard = popupAddCard.querySelector('.popup__close-cross');
 
 /* for already existing profile name and description */
 const nameProfile = document.querySelector('.profile__name');
 const descriptionProfile = document.querySelector('.profile__description');
+
 /* for name and description from form */
 const nameInput = document.querySelector('.popup__input_type_name');
 const descriptionInput = document.querySelector('.popup__input_type_description');
 
-const form = document.querySelector('.popup__form');
+/* forms */
+const formProfile = popupProfile.querySelector('.popup__form');
+const formAddCard = popupAddCard.querySelector('.popup__form');
+
+/* card name and link */
+const cardName = document.querySelector('.popup__input_type_card-name');
+const cardLink = document.querySelector('.popup__input_type_card-link');
 
 /* elements */
 const elementsList = document.querySelector(".elements__list");
 const elementTemplate = document.querySelector("#element-template").content.querySelector('.element');
 const element = elementTemplate.querySelector(".element");
 
-/* show popup and get name and description from profile*/
-function showPopup() {
-    nameInput.value = nameProfile.textContent;
-    descriptionInput.value = descriptionProfile.textContent;
+function showPopup(popup) {
     popup.classList.add('popup_active');
 }
 
-function closePopup() {
+/* show popup and get name and description from profile*/
+function showPopupProfile() {
+    nameInput.value = nameProfile.textContent;
+    descriptionInput.value = descriptionProfile.textContent;
+    showPopup(popupProfile);
+}
+
+function closePopup(popup) {
     popup.classList.remove('popup_active');
 }
 
@@ -57,16 +71,28 @@ function createInitCards(initCardsList) {
 
 createInitCards(initialCards);
 
-/* submit handler */
-function handleFormSubmit(evt) {
+/* submit handlers */
+function handleFormSubmitProfile(evt) {
     evt.preventDefault();
     nameProfile.textContent = nameInput.value;
     descriptionProfile.textContent = descriptionInput.value;
-    closePopup(popup);
+    closePopup(popupProfile);
+}
+
+function handleFormSubmitAddCard(evt) {
+    evt.preventDefault();
+    addCard(cardLink.value, cardName.value);
+    closePopup(popupAddCard);
 }
 
 /*event listeners*/
 /* listeners for buttons */
-editButton.addEventListener('click', showPopup);
-closeButton.addEventListener('click', closePopup);
-form.addEventListener('submit', handleFormSubmit);
+editButton.addEventListener('click', showPopupProfile);
+closeButtonProfile.addEventListener('click', () => closePopup(popupProfile));
+closeButtonCard.addEventListener('click', () => closePopup(popupAddCard));
+addButton.addEventListener('click', () => showPopup(popupAddCard));
+/* submit listeners */
+formProfile.addEventListener('submit', handleFormSubmitProfile);
+formAddCard.addEventListener('submit', handleFormSubmitAddCard);
+
+
